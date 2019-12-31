8bc7aaa1a3a8e2ff199a7c36e5da99824ae32346
mako <mako1980@users.noreply.github.com>
2019-12-31 09:00:23 +0900

    img以外も移動

--- move_files.rb
+++ move_files.rb
@@ -12,17 +12,21 @@ require 'fileutils'
 # TODO 別フォルダに移動したり、
 #      ゴミフォルダは削除するなど
 
-puts '--------------------'
-puts 'start'
-puts '--------------------'
-
 cur = Dir.pwd + '/'
 i = 0 # 連番用
-not_jpg = Array.new # jpg以外のリスト
+j = 0 # 連番用
+# not_jpg = Array.new # jpg以外のリスト
 
+# 画像の移動先フォルダを作成
 if !Dir.exist?('pic')
   Dir.mkdir('pic')
 end
+
+# 画像以外の移動先フォルダを作成
+if !Dir.exist?('other')
+  Dir.mkdir('other')
+end
+
 
 #-------------------------------------------------
 # ファイル名を変更しpic/年月へ移動
@@ -32,9 +36,9 @@ end
 Dir.glob('./**/*').each do |f|
 
   ext = File.extname(f) # 拡張子取得
-  if ext == '.JPG' ||
-     ext == '.jpg' ||
-     ext == '.jpeg'
+
+  # 画像
+  if ext == '.JPG' || ext == '.jpg' || ext == '.JPEG' || ext == '.jpeg'
 
     # 移動先ディレクトリ作成
     to_dir_name = cur + '/pic/' + File.mtime(f).strftime('%y%m')
@@ -42,25 +46,47 @@ Dir.glob('./**/*').each do |f|
       Dir.mkdir(to_dir_name)
     end
 
-    # 変更名が移動先に存在する場合、連番を振る
-    new_name = to_dir_name + '/' + File.mtime(f).strftime('%y%m%d_%H%M%S') + ext
+    # 変更名を作成
+    new_name = to_dir_name + '/IMG_' + File.mtime(f).strftime('%y%m%d_%H%M%S') + ext
     if File.exist?(new_name)
+      # 移動先に存在する場合、連番を振る
       i +=1
-      new_name = to_dir_name + '/' + File.mtime(f).strftime('%y%m%d_%H%M%S') + '_' + i.to_s + ext
+      new_name = to_dir_name + '/IMG_' + File.mtime(f).strftime('%y%m%d_%H%M%S') + '_' + i.to_s + ext
     end
 
-    # カレントディレクトリを変更（ブロックを抜けると戻る）
-    f_dir =  File.dirname(f)
-    FileUtils.chdir(f_dir) {
-      File.rename(File.basename(f), new_name)
-    }
+  # 画像以外
   else
-    not_jpg.push(f)
+    # 移動先ディレクトリ作成
+    to_dir_name = cur + '/other'
+    # 変更名を作成
+    new_name = to_dir_name + '/' + File.mtime(f).strftime('%y%m%d_%H%M%S') + ext
+    if File.exist?(new_name)
+      j +=1
+      # 移動先に存在する場合、連番を振る
+      new_name = to_dir_name + '/' + File.mtime(f).strftime('%y%m%d_%H%M%S') + '_' + j.to_s + ext
+    end
+    # not_jpg.push(f)
+  end
+
+  # 移動
+  if FileTest.file? f
+    if File.basename(f) == 'move_files.rb'
+    else
+      # カレントディレクトリを変更（ブロックを抜けると戻る）
+      FileUtils.chdir(File.dirname(f)) {
+        # ファイルを移動
+        File.rename(File.basename(f), new_name)
+      }
+    end
   end
 
 end
 
 puts '--------------------'
-puts 'not_jpg_list'
+puts 'done.'
 puts '--------------------'
-puts not_jpg
+
+# puts '--------------------'
+# puts 'not_jpg_list'
+# puts '--------------------'
+# puts not_jpg
